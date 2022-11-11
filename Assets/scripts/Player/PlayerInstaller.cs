af4a8eb3572bc9f1490ab95372946300d825323f
Zero <gosha@kolokutin.com>
2022-11-12 01:40:13 +0300

    Commit

--- Assets/scripts/Player/PlayerInstaller.cs
+++ Assets/scripts/Player/PlayerInstaller.cs
@@ -6,27 +6,22 @@ using Zenject;
 
 public class PlayerInstaller : MonoInstaller, IInitializable
 {
-    [SerializeField] private PlayerUnit PlayerUnit;
+    [SerializeField] private PlayerController Player;
     [SerializeField] private Transform playerSpawnPoint;
     [SerializeField] private VariableJoystick Joystick;
     [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
     [SerializeField] private HealthBar HealthBar;
     public override void InstallBindings()
     {
-        if(PlayerUnit.GetComponent<PlayerController>())
+        Container.BindInterfacesTo<PlayerInstaller>().FromInstance(this);
+        Container.Bind(typeof(HealthBar)).FromInstance()
+        if(Player.GetComponent<PlayerController>())
         {
-            PlayerUnit.GetComponent<PlayerController>().joystick = Joystick;
+            Player.GetComponent<PlayerController>().joystick = Joystick;
         }
         
-        var playerInstance = Container.InstantiatePrefabForComponent<PlayerUnit>(PlayerUnit, playerSpawnPoint.position, Quaternion.identity, null);
+        PlayerController playerInstance = Container.InstantiatePrefabForComponent<PlayerController>(Player, playerSpawnPoint.position, Quaternion.identity, null);
         Container.Bind<PlayerUnit>().FromInstance(playerInstance).AsSingle().NonLazy();
-        HealthBar.GetComponent<LookAtCam>().cam = cinemachineVirtualCamera.transform;
-        //Container.Bind<IDamageable>().To<HealthBar>().AsSingle();
-        var healthBarInstance = Container.InstantiatePrefabForComponent<HealthBar>(HealthBar, playerInstance.transform);
-        Container.Bind<HealthBar>().FromInstance(healthBarInstance).AsSingle().NonLazy();
-        
-        //Container.Bind<IDamageable>().To<HealthData>().AsSingle();
-        //Container.Bind<IDamageable>().To<Damageable>().FromInstance(new Damageable());
         cinemachineVirtualCamera.Follow = playerInstance.transform;
         
     }
